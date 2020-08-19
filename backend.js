e0f837be0da9ab742e4a598ad5af740b22a01708
Little Whale <vladimirputin420@protonmail.com>
2020-08-19 12:42:31 +0300

    the backend layout is written

---
+++ backend.js
@@ -0,0 +1,86 @@
+const fs = require('fs')
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const path = "./data"
+
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json())
+
+
+// This are handlers for login and registation form
+app.get('/data', (req, res) => {
+    if (fs.existsSync(path)) {
+        fs.readFile('data/users.json', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            res.send(data)
+        });
+    } else {
+        fs.mkdirSync("data")
+        fs.writeFileSync('data/users.json', "[]")
+        fs.readFile('data/users.json', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            res.send(data)
+        });
+    }
+})
+app.post('/data', (req, res) => {
+    const arrayOfUsers = req.body;
+    if (fs.existsSync(path)) {
+        fs.writeFile('data/users.json', JSON.stringify(arrayOfUsers), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    } else {
+        fs.mkdirSync("data")
+        fs.writeFile('data/users.json', JSON.stringify(arrayOfUsers), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        })
+    }
+})
+// ------------------------------------------------
+
+// This are handlers for getting and sending wishes to DB
+app.get('/data', (req, res) => {
+    if (fs.existsSync(path)) {
+        fs.readFile('data/wishes.json', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            res.send(data)
+        });
+    } else {
+        fs.mkdirSync("data")
+        fs.writeFileSync('data/wishes.json', "[]")
+        fs.readFile('data/wishes.json', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+            res.send(data)
+        });
+    })
+
+app.post('/data', (req, res) => {
+    const arrayOfWishes = req.body;
+    if (fs.existsSync(path)) {
+        fs.writeFile('data/wishes.json', JSON.stringify(arrayOfWishes), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+    } else {
+        fs.mkdirSync("data")
+        fs.writeFile('data/wishes.json', JSON.stringify(arrayOfWishes), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        })
+    }
+})
+// ------------------------------------------------
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
---
+++ src/model.js
@@ -0,0 +1,27 @@
+export const createNewUser = (arrayOfUsers, userName, login, password) => {
+    const user = {
+        userName: userName,
+        login: login,
+        password: password,
+        isAdmin: false,
+        wishList: []
+    }
+    if (arrayOfUsers) arrayOfUsers.push(user)
+}
+
+export const createNewWish = (title, body, url, wishList) => {
+    const wish = {
+        title: title,
+        body: body,
+        url: url
+    }
+    if (wishList) wishList.push(wish)
+}
+
+export const deleteWish = (wishList, index) => wishList.splice(index, 1)
+
+export const editWishTitle = (wishList, index, title) => wishList[index].title = title
+
+export const editWishBody = (wishList, index, body) => wishList[index].body = body
+
+export const editWishUrl = (wishList, index, url) => wishList[index].url = url
