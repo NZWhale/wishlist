375496587b83f0f84a0a0ed92b357b6c1103f1c7
Little Whale <vladimirputin420@protonmail.com>
2020-08-22 14:53:11 +0300

    wishes editing implemented

--- src/htmlUtils.js
+++ src/htmlUtils.js
@@ -1,4 +1,4 @@
-import { createNewWish, deleteWish } from "./model"
+import { createNewWish, deleteWish, editWishTitle, editWishBody, editWishUrl } from "./model"
 import { renderWishes } from "./view"
 import {backendURL, fetchPostRequest} from "./utils"
 
@@ -32,42 +32,47 @@ export const createWishElement = (wish, index) => {
     const wishTitle = document.createElement("div")
     const wishBody = document.createElement("div")
     const wishUrl = document.createElement("div")
-    const checkBox = createCheckbox(index)
-    const deleteButton = document.createElement("button")
+    const deleteButton = createInput("button", "delete", "deleteButton", "deleteButton")
     const deleteButtonImg = document.createElement("img")
+    const editButton = createInput("button", "edit", "editButton", "editButton")
+    const editButtonImg = document.createElement("img")
     deleteButton.appendChild(deleteButtonImg)
     wishDiv.setAttribute("id", "singleWish")
+    wishDiv.setAttribute("class", "singleWish")
     wishDiv.setAttribute("index", `${index}`)
     wishTitle.innerHTML = wish.title
     wishBody.innerHTML = wish.body
     wishUrl.innerHTML = wish.url
-    deleteButton.setAttribute("class", "deleteButton")
     deleteButton.addEventListener("click", function () {
         const wishIndex = wishDiv.getAttribute("index")
         deleteWish(wishList, wishIndex)
         renderWishes(wishList)
         fetchPostRequest("POST", backendURL, wishList)
     })
-    deleteButtonImg.setAttribute("src", "src/deleteButton.jpg")
+    deleteButtonImg.setAttribute("src", "./src/deleteButton.jpg")
     deleteButtonImg.setAttribute("class", "deleteButtonImg")
+    editButton.addEventListener("click", function(){
+        const wishIndex = wishDiv.getAttribute("index")
+        const editedTitle = prompt("enter title")
+        const editedBody = prompt("enter body")
+        const editedUrl = prompt("enter url")
+        editWishTitle(wishList, wishIndex, editedTitle)
+        editWishBody(wishList, wishIndex, editedBody)
+        editWishUrl(wishList, wishIndex, editedUrl)
+        renderWishes(wishList)
+        fetchPostRequest("POST", backendURL, wishList)
+    })
+    editButtonImg.setAttribute("src", "./src/editButton.jpg")
+    editButtonImg.setAttribute("class", "editButton")
+    deleteButton.append(deleteButtonImg)
+    editButton.append(editButtonImg)
     wishDiv.append(wishTitle)
     wishDiv.append(wishBody)
     wishDiv.append(wishUrl)
-    wishDiv.append(checkBox)
     wishDiv.append(deleteButton)
+    wishDiv.append(editButton)
     return wishDiv
 }
-
-// export const deleteWishElement = (wishList, index) => {
-//     const checkedCheckBox = document.getElementsByClassName("checkbox")
-//     checkedCheckBox.forEach(checkbox => {
-//         if(checkbox.checked){
-//              return checkbox.index
-//         }
-//     });
-//     deleteWish(wishList, checkboxIndex)
-//     renderWishes(wishList)
-// }
 
 export const clearTextArea = () => {
     document.getElementById("titleArea").value = ""
